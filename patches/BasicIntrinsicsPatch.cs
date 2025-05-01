f132fe2f17e770117d47780e3dd38142c380c6b4
Macha Celeste <shetah1@yahoo.com>
2025-05-01 15:38:50 -0400

    added check for intrinsicsAdded fixed mod formatting

--- patches/BasicIntrinsicsPatch.cs
+++ patches/BasicIntrinsicsPatch.cs
@@ -5,63 +5,71 @@ using System.Linq;
 using System.Text.RegularExpressions;
 
 [HarmonyPatch]
-class BasicIntrinsicsPatch
+public class BasicIntrinsicsPatch
 {
     [HarmonyPatch(typeof(BasicIntrinsics), "AddInstrinsics")]
-    static void Postfix()
+    class AddInstrinsicsPatch
     {
-        Intrinsic intrinsic6 = Intrinsic.Create("matches");
-        intrinsic6.AddParam("self");
-        intrinsic6.AddParam("pattern");
-        intrinsic6.AddParam("regexOptions", "none");
-        intrinsic6.code = delegate (TAC.Context context, Intrinsic.Result partialResult)
+        private static bool intrinsicsAdded;
+
+        static void Postfix()
         {
-            Value var = context.GetVar("self");
-            ValString valString = context.GetVar("pattern") as ValString;
-            ValString valString2 = context.GetVar("regexOptions") as ValString;
-            if (var is ValString && valString != null && valString2 != null)
+            if (intrinsicsAdded == true)
+                return;
+            intrinsicsAdded = true;
+            Intrinsic intrinsic6 = Intrinsic.Create("matches");
+            intrinsic6.AddParam("self");
+            intrinsic6.AddParam("pattern");
+            intrinsic6.AddParam("regexOptions", "none");
+            intrinsic6.code = delegate (TAC.Context context, Intrinsic.Result partialResult)
             {
-                string value = ((ValString)var).value;
-                if (string.IsNullOrEmpty(valString.value))
+                Value var = context.GetVar("self");
+                ValString valString = context.GetVar("pattern") as ValString;
+                ValString valString2 = context.GetVar("regexOptions") as ValString;
+                if (var is ValString && valString != null && valString2 != null)
                 {
-                    throw new TypeException("Type Error: 'is_match' pattern can't be empty or null");
-                }
-                if (!new string[]
-                {
+                    string value = ((ValString)var).value;
+                    if (string.IsNullOrEmpty(valString.value))
+                    {
+                        throw new TypeException("Type Error: 'is_match' pattern can't be empty or null");
+                    }
+                    if (!new string[]
+                    {
                     "none",
                     "i",
                     "m",
                     "s",
                     "n",
                     "x"
-                }.Contains(valString2.value))
-                {
-                    throw new RuntimeException("matches: Invalid regex option");
-                }
-                TimeSpan matchTimeout = TimeSpan.FromMilliseconds(500.0);
-                RegexOptions options = valString2.value.ToRegexOptions();
-                try
-                {
-                    MatchCollection matchCollection = Regex.Matches(value, valString.value, options, matchTimeout);
-                    ValList valList = new ValList();
-                    foreach (object obj in matchCollection)
+                    }.Contains(valString2.value))
                     {
-                        Match match = (Match)obj;
-                        ValList valList2 = new ValList();
-                        foreach (object grp in match.Groups)
-                        {
-                            valList2.values.Add(new ValString(grp.ToString()));
-                        }
-                        valList.values.Add(valList2);
+                        throw new RuntimeException("matches: Invalid regex option");
                     }
-                    return new Intrinsic.Result(valList, true);
+                    TimeSpan matchTimeout = TimeSpan.FromMilliseconds(500.0);
+                    RegexOptions options = valString2.value.ToRegexOptions();
+                    try
+                    {
+                        MatchCollection matchCollection = Regex.Matches(value, valString.value, options, matchTimeout);
+                        ValList valList = new ValList();
+                        foreach (object obj in matchCollection)
+                        {
+                            Match match = (Match)obj;
+                            ValList valList2 = new ValList();
+                            foreach (object grp in match.Groups)
+                            {
+                                valList2.values.Add(new ValString(grp.ToString()));
+                            }
+                            valList.values.Add(valList2);
+                        }
+                        return new Intrinsic.Result(valList, true);
+                    }
+                    catch (RegexMatchTimeoutException)
+                    {
+                        throw new RuntimeException("matches: operation timeout");
+                    }
                 }
-                catch (RegexMatchTimeoutException)
-                {
-                    throw new RuntimeException("matches: operation timeout");
-                }
-            }
-            throw new TypeException("Type Error: 'matches' requires string");
-        };
+                throw new TypeException("Type Error: 'matches' requires string");
+            };
+        }
     }
 }
